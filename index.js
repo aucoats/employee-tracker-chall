1c1de101d13665c64d7f6381d38e35ddd45b5492
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-01-07 13:54:15 -0500

    db connection

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
- 
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'employee_tracker',
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected');
+})
+
+
